7ea64a32c42cb08980eb4e623e24d422eb1c5057
nestibry <nestibry@gmail.com>
2023-11-09 15:53:51 -0600

    init model index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,50 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Category = require('./Category');
 
-module.exports = { User };
+// User has many Posts && Post belongs to User
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+// Post has many comments && Comments belong to post
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+
+// User has many comments && Comment belongs to User
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+// Category has many Posts && Post belongs to Category
+Category.hasMany(Post, {
+    foreignKey: 'category_id',
+    onDelete: 'SET NULL',
+});
+
+Post.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+
+module.exports = { User, Post, Comment, Category };
