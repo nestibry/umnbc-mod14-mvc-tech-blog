7ed6b0d69c43de46a2ea6f8b6050ee74eb90f5f8
nestibry <nestibry@gmail.com>
2023-11-10 09:12:18 -0600

    init devRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+require('dotenv').config();
 
 router.use('/users', userRoutes);
+
+if(process.env.NODE_ENV === 'dev'){
+    const devRoutes = require('./devRoutes');
+    router.use('/dev', devRoutes);
+}
 
 module.exports = router;
