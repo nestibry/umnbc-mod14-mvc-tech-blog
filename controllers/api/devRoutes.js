1747e08d7ecb02906a8eefa95635041f666f1fc4
nestibry <nestibry@gmail.com>
2023-11-10 10:53:23 -0600

    tried an include statement with Category.findAll()

--- controllers/api/devRoutes.js
+++ controllers/api/devRoutes.js
@@ -9,8 +9,9 @@ router.get('/category', async (req, res) => {
     // be sure to include its associated Products
     try {
         const data = await Category.findAll({
-            include: [{ model: Post }]
+            // include: [{ model: Post }]
             // include: [{ model: Post }, {model: Comment, through: Post, as:'comment'}]
+            // include: {model: Comment, include: Post}
         });
         res.status(200).json(data);
     } catch (err) {
