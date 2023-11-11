0a940ffc0d27405c4663a2750848c4fc7fe1e414
nestibry <nestibry@gmail.com>
2023-11-11 16:38:38 -0600

    intialized devCommentRoutes

--- controllers/api/dev/index.js
+++ controllers/api/dev/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 const devUserRoutes = require('./devUserRoutes');
 const devCategoryRoutes = require('./devCategoryRoutes');
 const devPostRoutes = require('./devPostRoutes');
+const devCommentRoutes = require('./devCommentRoutes');
 
 router.use('/users', devUserRoutes);
 router.use('/categories', devCategoryRoutes);
 router.use('/posts', devPostRoutes);
+router.use('/comments', devCommentRoutes);
 
 module.exports = router;
