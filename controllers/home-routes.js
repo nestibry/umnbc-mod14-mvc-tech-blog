44fd1d1d9418ce2537797da338c91e83ad0fd0f5
nestibry <nestibry@gmail.com>
2023-11-15 04:44:54 -0600

    new home route added logged_in to the serialize to handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { User, Category, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', async (req, res) => {
+    try {
+        // Get all RECORDS and JOIN with other data
+        const data = await Post.findAll({
+            attributes: ['title', 'content', 'createdAt', 'updatedAt'],
+            include: [
+                { model: User, attributes: ['name'] },
+                { model: Category, attributes: ['title'] },
+                { model: Comment, attributes: ['content'], include: { model: User, attributes: ['name', 'createdAt'] } }
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = data.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', { posts, logged_in: req.session.logged_in });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+
+module.exports = router;
