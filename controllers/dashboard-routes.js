188e31f6007c21af671c07773526519e67b833ec
nestibry <nestibry@gmail.com>
2023-11-18 16:13:20 -0600

    changed how withAuth is exported and imported

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Category, Post, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
+const { withAuth } = require('../utils/auth');
 
 
 // Render the Dashboard by user_id
