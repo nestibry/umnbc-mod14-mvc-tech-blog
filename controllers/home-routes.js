d418df3413115e3249ba5d7e77941b4d956e6e58
nestibry <nestibry@gmail.com>
2023-11-18 20:44:56 -0600

    intialized a new-comment handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -57,6 +57,31 @@ router.get('/post/:id', async (req, res) => {
 });
 
 
+
+// Render the new-comment form
+router.get('/new-comment/post/:id', withAuth, async (req, res) => {
+    // Find record by ID and include other model data
+    try {
+        const data = await Post.findByPk(req.params.id, {
+            attributes: ['id', 'title'],
+        });
+        // Return an error if record not found
+        if (!data) {
+            res.status(404).json({ message: 'Record ' + req.params.id + ' not found.' });
+            return;
+        }
+
+        // Serialize data so the template can read it
+        const post = data.get({ plain: true });
+
+        // Pass serialized data and session flag into template
+        res.render('new-comment', { ...post, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
